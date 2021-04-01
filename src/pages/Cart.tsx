3721aa8ceab90411244236f36459caf6475386bd
iamhabbeboy <iamhabbeboy@gmail.com>
2021-04-02 00:46:14 +0100

    build: setup tailwindcss, react router

---
+++ src/pages/Cart.tsx
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+const Cart: React.FC = () => {
+    return (
+        <div className="container mx-auto">
+            <div className="text-white">Cart here</div>
+        </div>
+    );
+};
+
+export default Cart;
