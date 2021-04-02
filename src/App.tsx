00f41d97d1e6b36b63079c003c77a78ecc08fc37
iamhabbeboy <iamhabbeboy@gmail.com>
2021-04-03 00:25:34 +0100

    feat: add components and fix tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Routes from './Routes';
-import { BrowserRouter as Router } from 'react-router-dom';
+import React from "react";
+import Routes from "./Routes";
+import Header from "./components/Header";
+import { BrowserRouter as Router } from "react-router-dom";
 
 const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+      <Header></Header>
+      <section>
         <Router>
-              <h1>jhkhhkhj</h1>
-              <Routes />
-            </Router>
-      </header>
+          <Routes />
+        </Router>
+      </section>
     </div>
   );
-}
+};
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,25 @@
+const colors = require("tailwindcss/colors");
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      red: colors.red,
+      black: colors.black,
+      white: colors.white,
+      gray: colors.coolGray,
+      purple: colors.purple,
+    },
+    fontSize: {
+      sm: "0.275rem",
+      md: "1rem",
+      lg: "1.25rem",
+      xl: "1.5rem",
+    },
     extend: {},
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
